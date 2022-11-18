2e58640b9c184f4af3b065c7bac47bcf9185df66
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2022-11-18 19:01:17 +0530

    Added some content

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import NotificationBar from './components/NotificationBar';
 import { notifications } from './utils/notificationsData';
 
 function App() {
+  const unreadNotifications = notifications.filter((notification) => {
+    return !notification.isRead;
+  }).length;
+
   return (
     <div className="main">
       <div className="app">
         <div className="notification-bar">
-          <NotificationBar />
+          <NotificationBar count={unreadNotifications} />
         </div>
         <div className="message-box">
           {notifications?.map((data) => (
--- src/components/NotificationBar.js
+++ src/components/NotificationBar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './NotificationBar.css';
 
-const NotificationBar = () => {
-  return <div>Notification</div>;
+const NotificationBar = ({ count }) => {
+  return (
+    <div className="notification-bar-main">
+      <div className="notification-bar-main-header">
+        <h2 className="notification-bar-main-header-tag">Notifications</h2>
+        <div className="notification-bar-main-header-unreadCount">{count}</div>
+      </div>
+      <div className="notification-bar-main-markAsRead">Mark all as Read</div>
+    </div>
+  );
 };
 
 export default NotificationBar;
