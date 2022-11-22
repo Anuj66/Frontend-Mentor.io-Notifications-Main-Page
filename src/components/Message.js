dea91c49789600b56cdc2d61015c707d53fc8abd
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2022-11-22 15:13:12 +0530

    Added message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import './Message.css';
+import RedDot from '../assets/images/reddot.png';
 
 const Message = ({ data }) => {
-  return <div>{data?.sentBy}</div>;
+  return (
+    <>
+      <div className={`message-div ${data?.isRead ? '' : 'message-content-read'}`}>
+        <div className="message-avatar">
+          <img src={data?.avatar} alt="Avatar" className="message-avatar-img" />
+        </div>
+        <div className={`message-content`}>
+          <p className="message-content-p">
+            {data?.sentBy && (
+              <span className="message-content-p-span-sentBy">{data?.sentBy}</span>
+            )}
+            {data?.message && (
+              <span className="message-content-p-span-message">{data?.message}</span>
+            )}
+            {data?.highlightedMessage && (
+              <span
+                className={`message-content-p-span-highlightedMessage ${
+                  data?.color === 'darkBlue'
+                    ? 'message-content-p-span-highlightedMessage-darkBlue'
+                    : 'message-content-p-span-highlightedMessage-blue'
+                }`}
+              >
+                {data?.highlightedMessage}
+              </span>
+            )}
+            {!data?.isRead && (
+              <span className="message-content-p-span-reddot">
+                <img
+                  src={RedDot}
+                  alt="reddot"
+                  className="message-content-p-span-reddot-img"
+                />
+              </span>
+            )}
+          </p>
+          <p className="message-content-p-sentAt">{data?.sentAt}</p>
+        </div>
+      </div>
+      {data?.privateMessage && (
+        <div className="message-content-privateMessage">
+          <p className="message-content-privateMessage-p">{data?.privateMessage}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Message;
--- src/components/NotificationBar.js
+++ src/components/NotificationBar.js
@@ -6,7 +6,7 @@ const NotificationBar = ({ count }) => {
     <div className="notification-bar-main">
       <div className="notification-bar-main-header">
         <h2 className="notification-bar-main-header-tag">Notifications</h2>
-        <div className="notification-bar-main-header-unreadCount">{count}</div>
+        <p className="notification-bar-main-header-unreadCount">{count}</p>
       </div>
       <div className="notification-bar-main-markAsRead">Mark all as Read</div>
     </div>
